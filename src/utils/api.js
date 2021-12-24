686259c823c3486013d2bf9934bd4eea4dc9f6e6
Aliaksandr Karpovich <alexsure.k@gmail.com>
2021-12-24 14:36:05 +0300

    configs & api

---
+++ src/utils/api.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+import { API_URL } from "../config";
+import { getToken } from "./session";
+
+let headers = {
+    'Content-Type': 'application/json'
+};
+
+const token = getToken();
+
+if (token) {
+    headers['Authorization'] = `Bearer ${token}`;
+}
+
+export default axios.create({
+    baseURL: API_URL,
+    // timeout: 5000,
+    headers
+});
